02989ce58a609f19b33c2d03aea0c0749e98db10
ceosmore <ceosmore@gmail.com>
2024-04-26 17:37:28 -0500

    feat: :sparkles: Add cookies to accept and reject cookies

--- src/redux/actions/cookies/cookies.js
+++ src/redux/actions/cookies/cookies.js
@@ -1,26 +1,13 @@
-/* eslint-disable default-param-last */
-import { ACCEPT_COOKIES, REJECT_COOKIES } from '../actions/cookies/types';
+import { ACCEPT_COOKIES, REJECT_COOKIES } from './types';
 
-const initialState = {
-    cookies: null,
+export const acceptCookies = () => async (dispatch) => {
+    dispatch({
+        type: ACCEPT_COOKIES,
+    });
 };
 
-export default function cookies(state = initialState, action) {
-    // eslint-disable-next-line
-    const { type, payload } = action;
-
-    switch (type) {
-        case ACCEPT_COOKIES:
-            return {
-                ...state,
-                cookies: true,
-            };
-        case REJECT_COOKIES:
-            return {
-                ...state,
-                cookies: false,
-            };
-        default:
-            return state;
-    }
-}
+export const rejectCookies = () => async (dispatch) => {
+    dispatch({
+        type: REJECT_COOKIES,
+    });
+};
