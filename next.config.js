956ddb06e36b79f3f2bc31c813bd512a814c6329
ceosmore <ceosmore@gmail.com>
2024-04-26 12:39:17 -0500

    feat: :sparkles: Config next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    images: {
+        domains: [
+            'images.unsplash.com',
+            'tailwindui.com',
+            'bafybeicgamofiuvkc6wjxl4wwzzh6pdovhcvvyc2gw5verruiolnykzz3i.ipfs.w3s.link',
+        ],
+    },
+};
+
+module.exports = nextConfig;
