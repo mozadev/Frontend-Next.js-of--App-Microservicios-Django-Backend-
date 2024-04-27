aaa84eb69700b430ecf2ff8e9792b0ebffc8c54a
ceosmore <ceosmore@gmail.com>
2024-04-26 23:11:39 -0500

    feat: :sparkles: Add basic structure navbar, content and footer in src-hocs-Layout.jsx

---
+++ src/hocs/Layout.jsx
@@ -0,0 +1,16 @@
+import Footer from '@/features/footer';
+import Navbar from '@/features/navbar';
+
+export default function Layout({ children }) {
+    return (
+        <div className="dark:bg-dark-bg">
+            <Navbar />
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+                {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+                <div className="mx-auto max-w-7xl">{children}</div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
