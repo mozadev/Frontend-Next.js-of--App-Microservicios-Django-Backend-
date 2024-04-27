1cdaaf2508d6b359359a074c1942dc4e297430d0
ceosmore <ceosmore@gmail.com>
2024-04-26 20:31:33 -0500

    feat: :sparkles: Add Provider y config store en _app.jsx (Integrating Redux with react-redux and use custom wrapper to ensure acces to Redux  Store )

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import '@styles/globals.css'
 
+import { Provider } from 'react-redux';
+import { wrapper } from '@/redux/store';
+
+
 export default function App({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page);
-    return <>{getLayout(<Component {...pageProps} />)}</>
+    const { store, props } = wrapper.useWrappedStore(pageProps);
+
+    return <Provider store={store}>{getLayout(<Component {...props} />)}</Provider>
 }
