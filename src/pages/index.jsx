0073a80e5d828e7ee473f5bdef5c4d8568d33fb5
ceosmore <ceosmore@gmail.com>
2024-04-29 18:13:35 -0500

    fix: :lipstick: Fix on src-pages-index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,58 +24,55 @@ const SeoList = {
 
 export default function Home() {
 
-    // return (
-    //     <>
-    //         <Head>
-    //             <title>{SeoList.title}</title>
-    //             <meta name="description" content={SeoList.description} />
+    return (
+        <>
+            <Head>
+                <title>{SeoList.title}</title>
+                <meta name="description" content={SeoList.description} />
 
-    //             <meta name="keywords" content={SeoList.keywords} />
-    //             <link rel="canonical" href={SeoList.href} />
-    //             <meta name="robots" content={SeoList.robots} />
-    //             <meta name="author" content={SeoList.author} />
-    //             <meta name="publisher" content={SeoList.publisher} />
-    //             <meta name="viewport" content="width=device-width, initial-scale=1" />
-    //             <link rel="icon" href="/favicon.ico" />
+                <meta name="keywords" content={SeoList.keywords} />
+                <link rel="canonical" href={SeoList.href} />
+                <meta name="robots" content={SeoList.robots} />
+                <meta name="author" content={SeoList.author} />
+                <meta name="publisher" content={SeoList.publisher} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
 
-    //             {/* Social Media Tags */}
-    //             <meta property="og:title" content={SeoList.title} />
-    //             <meta property="og:description" content={SeoList.description} />
-    //             <meta property="og:url" content={SeoList.url} />
-    //             <meta property="og:image" content={SeoList.image} />
-    //             <meta property="og:image:width" content="1370" />
-    //             <meta property="og:image:height" content="849" />
-    //             <meta property="og:image:alt" content={SeoList.image} />
-    //             <meta property="og:type" content="website" />
+                {/* Social Media Tags */}
+                <meta property="og:title" content={SeoList.title} />
+                <meta property="og:description" content={SeoList.description} />
+                <meta property="og:url" content={SeoList.url} />
+                <meta property="og:image" content={SeoList.image} />
+                <meta property="og:image:width" content="1370" />
+                <meta property="og:image:height" content="849" />
+                <meta property="og:image:alt" content={SeoList.image} />
+                <meta property="og:type" content="website" />
 
-    //             {/* <meta property="fb:app_id" content="" /> */}
+                {/* <meta property="fb:app_id" content="" /> */}
 
-    //             {/* Video meta tags */}
-    //             <meta property="og:video" content={SeoList.video} />
-    //             <meta property="og:video:url" content={SeoList.video} />
-    //             <meta property="og:video:type" content="video/mp4" />
-    //             <meta property="og:video:width" content="1280" />
-    //             <meta property="og:video:height" content="720" />
-    //             <meta property="og:video:secure_url" content={SeoList.video} />
+                {/* Video meta tags */}
+                <meta property="og:video" content={SeoList.video} />
+                <meta property="og:video:url" content={SeoList.video} />
+                <meta property="og:video:type" content="video/mp4" />
+                <meta property="og:video:width" content="1280" />
+                <meta property="og:video:height" content="720" />
+                <meta property="og:video:secure_url" content={SeoList.video} />
 
-    //             {/* Twitter meta Tags */}
-    //             <meta name="twitter:title" content={SeoList.title} />
-    //             <meta name="twitter:description" content={SeoList.description} />
-    //             <meta name="twitter:image" content={SeoList.image} />
-    //             <meta name="twitter:card" content="summary_large_image" />
-    //             <meta name="twitter:site" content={SeoList.twitterHandle} />
-    //             <meta name="twitter:player:width" content="1280" />
-    //             <meta name="twitter:player:height" content="720" />
-    //             <meta name="twitter:player:stream" content={SeoList.video} />
-    //         </Head>
-    //         <div className="dark:bg-dark-bg">
-    //             {/* <Header /> */}
-    //             {/* <Features />
-    //             <Roadmap /> */}
-    //         </div>
-    //     </>
-    // );
-    return <div>Home Page</div>;
+                {/* Twitter meta Tags */}
+                <meta name="twitter:title" content={SeoList.title} />
+                <meta name="twitter:description" content={SeoList.description} />
+                <meta name="twitter:image" content={SeoList.image} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content={SeoList.twitterHandle} />
+                <meta name="twitter:player:width" content="1280" />
+                <meta name="twitter:player:height" content="720" />
+                <meta name="twitter:player:stream" content={SeoList.video} />
+            </Head>
+            <div>Home Page</div>;
+
+        </>
+    );
+
 }
 
 Home.getLayout = function getLayout(page) {
